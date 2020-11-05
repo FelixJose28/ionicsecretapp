4121251e289d29f67ad80e825aa6452c442b90fc
Felix Jose <felixjoseynfantequezada@gmail.com>
2020-11-05 11:45:27 -0400

    diseno basico, se puede ir conectando con la api

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private URL ='http://127.0.0.1:8000/api'
+
+  constructor(private http: HttpClient,
+              private router: Router) { }
+
+  authregistrar(user){
+    return this.http.post<any>(this.URL+'/registrar',user);
+  }
+  authlogearse(user){
+    return this.http.post<any>(this.URL+'/login',user);
+  }
+  authestaLoggeado(){
+    if(localStorage.getItem('token') != null){
+      return true
+    }
+  }
+  authGetToken(){
+    return localStorage.getItem('token')
+  }
+  authLogout(){
+    if(window.confirm('Desea cerrar la session??')){
+      localStorage.removeItem('token')
+      this.router.navigate['/loggin']
+    }
+  }
+  authCambiarDatos(user){
+    return this.http.put<any>(this.URL+'/modificardatosusuario/'+user.id,user)
+  }
 }
--- src/app/services/secretos.service.ts
+++ src/app/services/secretos.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SecretosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private URL = 'http://127.0.0.1:8000/api';
+
+
+    authgetSecreto(){
+      return this.http.get<any>(this.URL+ '/verSecretos?token='+localStorage.getItem('token'))
+    }
+    authregistrarSecreto(secreto){
+      return this.http.post<any>(this.URL+'/usuario/registrarSecreto',secreto)
+    }
+    authborrarSecreto(id) {
+      return this.http.delete<any>(this.URL + '/eliminarSecreto/'+id+'?token='+localStorage.getItem('token'))
+    }
 }
